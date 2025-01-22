b4c73da23da6c9f1510866cf3bf8ec78fedc8833
skilledify <skilledify@mail.ru>
2025-01-21 17:06:28 -0800

    fix gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -138,7 +138,7 @@ function watch() {
   gulp.watch(paths.html.src, html);
   gulp.watch(paths.styles.src, styles);
   gulp.watch(paths.scripts.src, scripts);
-  gulp.watch(paths.images.src, img);
+  gulp.watch(paths.img.src, img);
 }
 
 // Экспорт задач
